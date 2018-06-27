124b1bb648d5938e404cad9b6b26eaa4bb5b7ba9
Zosit <zosit123@hotmail.com>
2018-06-27 15:39:01 -0700

    Introduced tests and code for movement of basic pieces, and turn based game-play.

--- checkers/source/board.py
+++ checkers/source/board.py
@@ -1,16 +1,84 @@
 import numpy as np
 
-def initilize():
-	global board
-	board = np.zeros((10, 10))
-	for i in range(10):
-		for j in range(10):
-			if (((i + j) % 2) == 1):
-				board[i][j] = 1
+class CheckersGame:
+	def __init__(self, board_state, first_player):
+		global _board, _cur_player
+		_board = board_state
+		_cur_player = first_player
+		
+	@classmethod
+	def default_init(cls):
+		_init_board = np.zeros((8, 8))
+		for i in range(3):
+			for j in range(8):
+				if (((i + j) % 2) == 1):
+					_init_board[i][j] = 1
+
+		for i in range(3):
+			for j in range(8):
+				if (((i + 5 + j) % 2) == 1):
+					_init_board[i + 5][j] = 2
+		return cls(_init_board, 1)
+
+	def board_size(self):
+		return _board.shape
+		
+		
+	def val_at_loc(self, x, y):
+		return _board[x][y]
+	
+	
+	def player_turn(self):
+		return _cur_player
 
 	
-def board_size():
-    return board.shape
-	
-def val_at_loc(i, j):
-    return board[i][j]
+	def move_piece(self, x1, y1, x2, y2):
+		global _cur_player
+		if((x1 < 0) or (x1 > 7) or (y1 < 0) or (y1 > 7)):
+			raise Exception('Invalid location for target piece')
+		elif((x2 < 0) or (x2 > 7) or (y2 < 0) or (y2 > 7)):
+			raise Exception('Invalid location for piece to move to')
+		elif(_board[x1][y1] == 0):
+			raise Exception('There is no piece to move')
+		elif((_board[x1][y1] == 1) and (_cur_player == 1)):
+			if(((x2 == (x1 + 1)) or (x2 == (x1 - 1))) and (y2 == y1 + 1)):
+				if(_board[x2][y2] != 0):
+					raise Exception('There is another piece occupying the target location')
+				else:
+					_board[x2][y2] = 1
+					_board[x1][y1] = 0
+					_cur_player = 2
+			elif(((x2 == (x1 + 2)) or (x2 == (x1 - 2))) and (y2 == y1 + 2)):
+				if(_board[x2][y2] != 0):
+					raise Exception('There is another piece occupying the target location')
+				else:
+					if((_board[(x1 + x2) // 2][(y1 + y2) // 2]) != 2):
+						raise Exception('There is no enemy piece to jump over!')
+					else:
+						_board[x2][y2] = 1
+						_board[x1][y1] = 0
+						_board[(x1 + x2) // 2][(y1 + y2) // 2] = 0
+						_cur_player = 2
+			else:
+				raise Exception('invalid location to move piece')		
+		else:
+			if(((x2 == (x1 + 1)) or (x2 == (x1 - 1))) and (y2 == y1 - 1)):
+				if(_board[x2][y2] != 0):
+					raise Exception('There is another piece occupying the target location')
+				else:
+					_board[x2][y2] = 2
+					_board[x1][y1] = 0
+					_cur_player = 1
+			elif(((x2 == (x1 + 2)) or (x2 == (x1 - 2))) and (y2 == y1 - 2)):
+				if(_board[x2][y2] != 0):
+					raise Exception('There is another piece occupying the target location')
+				else:
+					if(_board[(x1 + x2) // 2][(y1 + y2) // 2] != 1):
+						raise Exception('There is no enemy piece to jump over!')
+					else:
+						_board[x2][y2] = 2
+						_board[x1][y1] = 0
+						_board[(x1 + x2) // 2][(y1 + y2) // 2] = 0
+						_cur_player = 1
+			else:
+				raise Exception('invalid location to move piece')	
--- checkers/tests/test.py
+++ checkers/tests/test.py
@@ -1,21 +1,129 @@
 import unittest
 from checkers.source import board
+import numpy as np
 
 class MyFirstTests(unittest.TestCase):
 	
 	def test_board_size(self):
-		board.initilize()
-		self.assertEqual(board.board_size(), (10, 10))
+		game = board.CheckersGame.default_init()
+		self.assertEqual(game.board_size(), (8, 8))
 		
-	def test_board_init_state(self):
-		board.initilize()
-		for i in range(10):
-			for j in range(10):
+	def test_board_default_state(self):
+		game = board.CheckersGame.default_init()
+		for i in range(3):
+			for j in range(8):
 				if (((i + j) % 2) == 1):
-					self.assertEqual(board.val_at_loc(i, j), 1)
-				else:
-					self.assertEqual(board.val_at_loc(i, j), 0)		
-					
-					
+					self.assertEqual(game.val_at_loc(i, j), 1)
+
+		for i in range(3):
+			for j in range(8):
+				if (((i + 5 + j) % 2) == 1):
+					self.assertEqual(game.val_at_loc(i + 5, j), 2)		
+		self.assertEqual(game.player_turn(), 1)
+
+		
+	def test_out_of_board_moves(self):
+		game = board.CheckersGame.default_init()
+		self.assertRaises(Exception, game.move_piece, 0, 1, -1, 2)
+		self.assertRaises(Exception, game.move_piece, -1, -1, 0, 0)
+
+	def test_missing_piece_move(self):
+		game = board.CheckersGame.default_init()
+		self.assertRaises(Exception, game.move_piece, 0, 0, 1, 1)
+
+	def test_move_collide(self):
+		game_state = np.zeros((8, 8))
+		game_state[0][1] = 1
+		game_state[1][2] = 1
+		game = board.CheckersGame(game_state, 1)
+		self.assertRaises(Exception, game.move_piece, 0, 1, 1, 2)
+
+	def test_legal_board_move(self):
+		game = board.CheckersGame.default_init()
+		game.move_piece(2, 1, 3, 2)
+		self.assertEqual(game.val_at_loc(2, 1), 0)
+		self.assertEqual(game.val_at_loc(3, 2), 1)
+		self.assertEqual(game.player_turn(), 2)
+		
+	def test_legal_jump_move(self):
+		game_state = np.zeros((8, 8))
+		game_state[0][1] = 1
+		game_state[1][2] = 2
+		game = board.CheckersGame(game_state, 1)
+		game.move_piece(0, 1, 2, 3)
+		self.assertEqual(game.val_at_loc(0, 1), 0)
+		self.assertEqual(game.val_at_loc(1, 2), 0)
+		self.assertEqual(game.val_at_loc(2, 3), 1)
+		self.assertEqual(game.player_turn(), 2)
+
+	def test_jump_collide(self):
+		game_state = np.zeros((8, 8))
+		game_state[0][1] = 1
+		game_state[1][2] = 2
+		game_state[2][3] = 1
+		game = board.CheckersGame(game_state, 1)
+		self.assertRaises(Exception, game.move_piece, 0, 1, 2, 3)
+
+	def test_jump_over_invalid(self):
+		game_state = np.zeros((8, 8))
+		game_state[0][1] = 1
+		game_state[1][2] = 1
+		game = board.CheckersGame(game_state, 1)
+		self.assertRaises(Exception, game.move_piece, 0, 1, 2, 3)
+
+	def test_move_location_invalid(self):
+		game_state = np.zeros((8, 8))
+		game_state[0][1] = 1
+		game = board.CheckersGame(game_state, 1)
+		self.assertRaises(Exception, game.move_piece, 0, 1, 1, 1)	
+	
+	def test_move_collide_reverse(self):
+		game_state = np.zeros((8, 8))
+		game_state[0][1] = 2
+		game_state[1][2] = 2
+		game = board.CheckersGame(game_state, 2)
+		self.assertRaises(Exception, game.move_piece, 1, 2, 0, 1)
+
+	def test_legal_board_move_reverse(self):
+		game_state = np.zeros((8, 8))
+		game_state[0][1] = 0
+		game_state[1][2] = 2
+		game = board.CheckersGame(game_state, 2)
+		game.move_piece(1, 2, 0, 1)
+		self.assertEqual(game.val_at_loc(1, 2), 0)
+		self.assertEqual(game.val_at_loc(0, 1), 2)
+		self.assertEqual(game.player_turn(), 1)
+		
+	def test_legal_jump_move_reverse(self):
+		game_state = np.zeros((8, 8))
+		game_state[1][2] = 1
+		game_state[2][3] = 2
+		game = board.CheckersGame(game_state, 2)
+		game.move_piece(2, 3, 0, 1)
+		self.assertEqual(game.val_at_loc(0, 1), 2)
+		self.assertEqual(game.val_at_loc(1, 2), 0)
+		self.assertEqual(game.val_at_loc(2, 3), 0)
+		self.assertEqual(game.player_turn(), 1)
+
+	def test_jump_collide_reverse(self):
+		game_state = np.zeros((8, 8))
+		game_state[0][1] = 2
+		game_state[1][2] = 1
+		game_state[2][3] = 2
+		game = board.CheckersGame(game_state, 2)
+		self.assertRaises(Exception, game.move_piece, 2, 3, 0, 1)
+
+	def test_jump_over_invalid_reverse(self):
+		game_state = np.zeros((8, 8))
+		game_state[1][2] = 2
+		game_state[2][3] = 2
+		game = board.CheckersGame(game_state, 2)
+		self.assertRaises(Exception, game.move_piece, 2, 3, 0, 1)
+
+	def test_move_location_invalid_reverse(self):
+		game_state = np.zeros((8, 8))
+		game_state[0][1] = 2
+		game = board.CheckersGame(game_state, 2)
+		self.assertRaises(Exception, game.move_piece, 0, 1, 1, 1)		
 if __name__ == '__main__':
     unittest.main()  
